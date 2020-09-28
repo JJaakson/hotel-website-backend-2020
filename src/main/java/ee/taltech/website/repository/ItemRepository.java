6701193e8561f1e1736642160e1af6a4abd94b1d
rjuur <rajuur@ttu.ee>
2020-09-28 10:54:58 +0300

    deleted unnessacary files #5

--- src/main/java/ee/taltech/website/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.taltech.website.repository;
-
-import ee.taltech.website.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Room, Integer> {
-}
