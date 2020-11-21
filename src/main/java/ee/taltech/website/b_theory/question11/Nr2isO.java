df7b7039f8033eaa5d90564886ce60f42c41a9e8
Rasmus Juurik <rajuur@ttu.ee>
2020-11-21 13:51:54 +0200

    theory B questions

--- src/main/java/ee/taltech/website/b_theory/question11/Nr2isO.java
+++ src/main/java/ee/taltech/website/b_theory/question11/Nr2isO.java
@@ -4,5 +4,5 @@ public class Nr2isO {
 
     //todo this is a contribution based question so make sure to keep commits separate
     //todo A What does O stand for in SOLID? Explain the principle.
-    //todo B Give an example. Write actual or pseudo code.
+    //todo B Give an example. Write actual or pseudo code.d
 }
